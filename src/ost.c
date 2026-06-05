d5b35f4c48134c65916eb17be1c40f1e0de008e5
vasily <vasily.stepanov@gmail.com>
2026-06-05 14:08:40 +0000

    fix: review cmd_process_set_object

--- src/ost.c
+++ src/ost.c
@@ -212,12 +212,10 @@ int push_block_write(conn_t* conn, proto_io_frame_t* frame, void* buf) {
     return 0;
 }
 
-int cmd_process_set_object(
-    conn_t* conn, int fd, uint16_t cid, void* data, int len
-) {
+int cmd_process_set_object(conn_t* conn, int fd, void* data, int len) {
     proto_basic_frame_t* frame = (proto_basic_frame_t*)data;
     int res = set_conn_params(conn, frame);
-    prep_and_send_resp_frame(fd, frame->cmd, cid, res);
+    prep_and_send_resp_frame(fd, frame->cmd, frame->cid, res);
     return 0;
 }
 
@@ -322,7 +320,7 @@ int process_recv(
                     );
                     goto partial_res;
                 }
-                cmd_process_set_object(conn, fd, mframe->cid, fdata, 0);
+                cmd_process_set_object(conn, fd, fdata, 0);
                 processed_size =
                     sizeof(proto_basic_frame_t) - conn->op_partial_offset;
                 goto next_iter;
