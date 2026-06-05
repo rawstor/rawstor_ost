4dfc285f503b46d0648aa88663ee0781ef088c17
vasily <vasily.stepanov@gmail.com>
2026-06-05 16:14:11 +0000

    fix: review commands_enum_t

--- src/ost.h
+++ src/ost.h
@@ -31,13 +31,13 @@
 // static const int OFFSET_BYTES = sizeof(uint16_t);
 
 // protocol section
-typedef enum {
+enum {
     CMD_SET_OBJECT = 0,
     CMD_READ = 1,
     CMD_WRITE = 2,
     CMD_DISCARD = 3,
     CMD_ALLOCATE_OBJ_CHUNK = 4,
-} commands_enum_t;
+};
 typedef uint16_t commands_t;
 
 /* Minimal protocol frame (cmd only) */
